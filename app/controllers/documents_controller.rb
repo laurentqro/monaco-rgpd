0b8a5e9af6fdfa38cff0a3b4e5cad20bbab6ad15
Laurent Curau <github@lqro.slmail.me>
2025-10-21 16:04:35 +0200

    fix: remove invalid authenticate_user! before_action from controllers
    ApplicationController already includes Authentication concern which provides
    require_authentication via before_action. No need to duplicate.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,4 @@
 class DocumentsController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     render inertia: 'Documents/Index', props: {
       documents: []
--- app/controllers/processing_activities_controller.rb
+++ app/controllers/processing_activities_controller.rb
@@ -1,6 +1,4 @@
 class ProcessingActivitiesController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     render inertia: 'ProcessingActivities/Index', props: {
       activities: []
