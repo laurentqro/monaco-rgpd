9879edf4dc8bdb1500f62b5945aae14eb0927890
Laurent Curau <github@lqro.slmail.me>
2025-11-05 17:51:39 +0100

    refactor: remove answer_value JSONB column

---
+++ db/migrate/20251105165125_remove_answer_value_from_answers.rb
@@ -0,0 +1,9 @@
+class RemoveAnswerValueFromAnswers < ActiveRecord::Migration[8.1]
+  def up
+    remove_column :answers, :answer_value
+  end
+
+  def down
+    add_column :answers, :answer_value, :jsonb, default: {}, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_05_165044) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_05_165125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -115,7 +115,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_05_165044) do
     t.decimal "answer_number", precision: 10, scale: 2
     t.integer "answer_rating"
     t.text "answer_text"
-    t.jsonb "answer_value", default: {}
     t.decimal "calculated_score", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.bigint "question_id", null: false
